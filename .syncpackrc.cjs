44350f64c1580789b9a722116c233e20d68fb2a4
Vasin Sermsampan <dev.vasin@gmail.com>
2023-11-29 21:17:14 +0700

    build(syncpack): update version groups

--- .syncpackrc.cjs
+++ .syncpackrc.cjs
@@ -3,8 +3,23 @@ module.exports = {
   semverRange: '^',
   source: [
     'package.json',
-    'app/**/package.json',
-    'config/**/package.json',
-    'libs/**/package.json',
+    'app/*/package.json',
+    'config/*/package.json',
+    'libs/*/package.json',
+  ],
+  versionGroups: [
+    {
+      label: 'Internal Workspace Version Group',
+      packages: ['**'],
+      dependencies: [
+        'config-prettier',
+        'config-tsconfig',
+        'config-tailwind',
+        'eslint-config-react',
+        'eslint-config-next',
+      ],
+      dependencyTypes: ['dev'],
+      pinVersion: 'workspace:*',
+    },
   ],
 };
