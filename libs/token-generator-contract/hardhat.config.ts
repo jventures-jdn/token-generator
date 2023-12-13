a996ffda4fd5a87b42d408d8927193b986dc1c23
Vasin Sermsampan <dev.vasin@gmail.com>
2023-12-13 14:29:06 +0700

    feat(token-generator-contract): new libs

---
+++ libs/token-generator-contract/hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from 'hardhat/config';
+import '@typechain/hardhat';
+import '@nomicfoundation/hardhat-ethers';
+import '@nomicfoundation/hardhat-chai-matchers';
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.17',
+  typechain: {
+    outDir: 'typechain',
+    target: 'ethers-v6',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: [], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+    dontOverrideCompile: false, // defaults to false
+  },
+};
+
+export default config;
