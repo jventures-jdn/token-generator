449bb20b6da0e05ccc455ccb9c494ec876cf4b96
Vasin Sermsampan <dev.vasin@gmail.com>
2023-12-13 17:49:25 +0700

    fix(token-generator-contract): ethers 6 force runtime to use node

--- libs/token-generator-contract/hardhat.config.ts
+++ libs/token-generator-contract/hardhat.config.ts
@@ -8,7 +8,7 @@ const config: HardhatUserConfig = {
   solidity: '0.8.17',
   typechain: {
     outDir: 'typechain',
-    target: 'ethers-v6',
+    target: 'ethers-v5',
     alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
     externalArtifacts: [], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
     dontOverrideCompile: false, // defaults to false
