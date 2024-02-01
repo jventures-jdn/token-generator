7d4b9b8afcb41f9e12d10f14ba7629a0fac393cd
Vasin Sermsampan <dev.vasin@gmail.com>
2024-02-01 11:10:28 +0700

    docs(token-generator-api.contract): function docs

--- apps/token-generator-api/src/modules/contract/contract.producer.ts
+++ apps/token-generator-api/src/modules/contract/contract.producer.ts
@@ -7,20 +7,50 @@ import { Job, Queue } from 'bull';
 export class ContractProducer {
   constructor(@InjectQueue('contract') private contractQueue: Queue) {}
 
+  /* ----------------------------- Add Compile Job ---------------------------- */
+  /**
+   * Add compile job to queue.
+   *
+   * @param {GeneratedContractDto} payload - Type and name of contract
+   * @returns Job id
+   */
   async addCompileJob(payload: GeneratedContractDto) {
     return (await this.contractQueue.add('compile', payload))?.id;
   }
 
+  /* ----------------------------- Add Verify Job ----------------------------- */
+  /**
+   * Add verify job to queue.
+   *
+   * @param {GeneratedContractDto} payload - Type and name of contract
+   * @returns Job id
+   */
   async addVerifyJob(payload: GeneratedContractDto) {
     return (await this.contractQueue.add('verify', payload))?.id;
   }
 
+  /* ------------------------------- Remove Job ------------------------------- */
+  /**
+   * Removes a job from queue.
+   *
+   * @param {JobDto} payload - The payload containing the jobId.
+   * @returns The removed job.
+   * @throws NotFoundException if the jobId does not exist.
+   */
   async removeJob(payload: JobDto) {
     const job = await this.getJob(payload);
     await job.remove();
     return job;
   }
 
+  /* --------------------------------- Get Job -------------------------------- */
+  /**
+   * Retrieves a job from the contract queue based on the provided payload.
+   *
+   * @param {JobDto} payload - The payload containing the jobId.
+   * @returns The job object if found.
+   * @throws NotFoundException if the jobId does not exist.
+   */
   async getJob(payload: JobDto) {
     const job: Job<GeneratedContractDto> = await this.contractQueue.getJob(
       payload.jobId,
@@ -35,6 +65,13 @@ export class ContractProducer {
     return job;
   }
 
+  /* ----------------------------- Get Job Status ----------------------------- */
+  /**
+   * Retrieves the status of a job.
+   *
+   * @param {Job<GeneratedContractDto>} payload - The job payload.
+   * @returns An object containing the job status properties.
+   */
   async getJobStatus(payload: Job<GeneratedContractDto>) {
     const [
       state,
