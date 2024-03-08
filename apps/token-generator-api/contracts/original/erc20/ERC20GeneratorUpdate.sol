c9e49e5b924f4fdfd264a5e65c000c5e02e88996
Vasin Sermsampan <dev.vasin@gmail.com>
2024-03-08 14:35:30 +0700

    perf(api.erc20): remove import

--- apps/token-generator-api/contracts/original/erc20/ERC20GeneratorUpdate.sol
+++ apps/token-generator-api/contracts/original/erc20/ERC20GeneratorUpdate.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.17;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/security/Pausable.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/access/AccessControl.sol';
 import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol';
 import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol';
