3b4fb66497e73a090b0ed29eecbf8443cff54b36
Vasin Sermsampan <dev.vasin@gmail.com>
2024-03-26 17:10:21 +0700

    perf(lib.react-component.input): textInput update

--- libs/react-component/src/input/TextInput.tsx
+++ libs/react-component/src/input/TextInput.tsx
@@ -1,6 +1,16 @@
-import { UseControllerProps, useController } from 'react-hook-form';
+import {
+  UseControllerProps,
+  UseFormSetValue,
+  UseFormUnregister,
+  useController,
+} from 'react-hook-form';
 import { InputTooltip, InputTooltipProps } from './Fragment';
-import { HTMLInputTypeAttribute } from 'react';
+import {
+  Dispatch,
+  HTMLInputTypeAttribute,
+  SetStateAction,
+  useEffect,
+} from 'react';
 
 export function TextInput<Inputs extends Record<string, any>>({
   controller,
@@ -9,13 +19,23 @@ export function TextInput<Inputs extends Record<string, any>>({
   title,
   placeholder,
   tooltip,
+  toggleFields,
+  hideMessage,
 }: {
-  controller: UseControllerProps<Inputs>;
+  controller: UseControllerProps<Inputs> & {
+    unregister?: UseFormUnregister<Inputs>;
+    setValue?: UseFormSetValue<Inputs>;
+  };
   type?: HTMLInputTypeAttribute;
   size?: 'xs' | 'sm' | 'md' | 'lg';
   title?: string;
   placeholder?: string;
   tooltip?: InputTooltipProps;
+  toggleFields?: {
+    setter: Dispatch<SetStateAction<{ [key in keyof Inputs]: boolean }>>;
+    value: { [key in keyof Inputs]: boolean };
+  };
+  hideMessage?: boolean;
 }) {
   /* -------------------------------------------------------------------------- */
   /*                                   States                                   */
@@ -27,30 +47,59 @@ export function TextInput<Inputs extends Record<string, any>>({
   const inputSize = size ? `!input-${size}` : '';
   const error = errors[controller.name];
   const message = error?.message as string | undefined;
+  const disableFieldState = toggleFields?.value[controller.name];
+
+  /* -------------------------------------------------------------------------- */
+  /*                                   Watches                                  */
+  /* -------------------------------------------------------------------------- */
+  // If input is disabled asuuming it is not required --> unregister validation
+  useEffect(() => {
+    if (toggleFields?.value[controller.name] === false) {
+      controller?.unregister?.('supplyCap' as never);
+    }
+  }, [disableFieldState]);
 
   /* -------------------------------------------------------------------------- */
   /*                                    Doms                                    */
   /* -------------------------------------------------------------------------- */
   return (
-    <div className="input-controller text-input">
+    <div className="input-container">
       {/* Title */}
       <div className="flex items-center gap-1 label-text ml-1">
         <label>{title}</label>
         <InputTooltip options={tooltip} />
+        {toggleFields && (
+          <input
+            checked={disableFieldState}
+            onChange={() =>
+              toggleFields.setter((s) => ({
+                ...s,
+                [controller.name]: !s[controller.name],
+              }))
+            }
+            type="checkbox"
+            className="ml-2 toggle toggle-sm"
+            disabled={field.disabled}
+          />
+        )}
       </div>
 
       {/* Input */}
       <input
         type={type}
         placeholder={placeholder || controller.name}
-        className={`input input-basic ${inputSize}${
-          message ? '!input-warning' : ''
+        className={`input text-input ${inputSize} ${
+          message ? '!input-error' : ''
         }`}
         {...field}
+        disabled={field.disabled || disableFieldState === false}
+        ref={field.ref}
       />
 
       {/* Message */}
-      {message && <p className="ml-1 text-xs text-warning/70">{message}</p>}
+      {!hideMessage && message && (
+        <p className="ml-1  text-xs text-error/70">{message}</p>
+      )}
     </div>
   );
 }
